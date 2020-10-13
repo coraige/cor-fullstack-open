0b2a5e74f20ee3c40685a45d7ca5e93f453f753f
coraige <36175291+coraige@users.noreply.github.com>
2020-10-14 02:43:42 +0300

    Add files via upload

---
+++ part1/courseinfo/build/precache-manifest.48651399f87ebef34ead684aec30cfa3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4718ee012e495ebd0ef95849e3e861ca",
+    "url": "/cor-fullstack-open/index.html"
+  },
+  {
+    "revision": "85ab979da649243ce7d1",
+    "url": "/cor-fullstack-open/static/js/2.b066b285.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cor-fullstack-open/static/js/2.b066b285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10a289d870f7a077886d",
+    "url": "/cor-fullstack-open/static/js/main.24c4e20f.chunk.js"
+  },
+  {
+    "revision": "a5238eddc69bd5dbc606",
+    "url": "/cor-fullstack-open/static/js/runtime-main.b5eee5f2.js"
+  }
+]);
